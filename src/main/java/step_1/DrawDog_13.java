f59ecce7150a3c97b71ee2bbc0ac613724beb1be
HE210401 <geamie@naver.com>
2023-05-13 21:08:26 +0900

    feat: 단계별(1) - [입출력과 사칙연산] - 13

---
+++ src/main/java/step_1/DrawDog_13.java
@@ -0,0 +1,11 @@
+package step_1;
+
+public class DrawDog_13 {
+    public static void main(String[] args) {
+        System.out.println("|\\_/| \n" +
+                           "|q p|   /} \n" +
+                           "( 0 )\"\"\"\\ \n" +
+                           "|\"^\"`    | \n" +
+                           "||_/=\\\\__|");
+    }
+}
