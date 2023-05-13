6975b9a7fae920fa14402b909dadf8b3b391b7f0
HE210401 <geamie@naver.com>
2023-05-13 20:48:16 +0900

    feat: 단계별(1) - [입출력과 사칙연산] - 9

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,9 +3,13 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int thailandYear = sc.nextInt();
-        int koreaYear = thailandYear - 543;
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
-        System.out.println(koreaYear);
+        System.out.println( (a + b) % c );
+        System.out.println( ((a % c) + (b % c)) % c );
+        System.out.println( (a * b) % c );
+        System.out.println( ((a % c) * (b % c)) % c );
     }
 }
