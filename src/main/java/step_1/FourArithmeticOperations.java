bcdba08d1ee1249a5da4dd9837e025e1ebd0ad34
HE210401 <geamie@naver.com>
2023-05-13 20:22:11 +0900

    feat: 단계별(1) - [입출력과 사칙연산] - 6

--- src/main/java/step_1/FourArithmeticOperations.java
+++ src/main/java/step_1/FourArithmeticOperations.java
@@ -1,8 +1,18 @@
 package step_1;
 
+import java.util.Scanner;
+
 public class FourArithmeticOperations {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
 
+        System.out.println(num1 + num2);
+        System.out.println(num1 - num2);
+        System.out.println(num1 * num2);
+        System.out.println(num1 / num2);
+        System.out.println(num1 % num2);
     }
 }
