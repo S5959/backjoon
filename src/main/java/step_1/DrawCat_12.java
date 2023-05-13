0df39a17d69c4a5b84046731258ea7cdeaaafe57
HE210401 <geamie@naver.com>
2023-05-13 21:07:46 +0900

    refactor: 단계별(1) - [입출력과 사칙연산] - 12

--- src/main/java/step_1/DrawCat_12.java
+++ src/main/java/step_1/DrawCat_12.java
@@ -2,9 +2,9 @@ package step_1;
 
 public class DrawCat_12 {
     public static void main(String[] args) {
-        System.out.println("\\    /\\\n" +
-                " )  ( ')\n" +
-                "(  /  )\n" +
-                " \\(__)|");
+        System.out.println("\\    /\\ \n" +
+                           " )  ( ') \n" +
+                           "(  /  ) \n" +
+                           " \\(__)|");
     }
 }
