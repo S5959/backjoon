845acd3ffe09d0f63984f47a89b77cc60027ca69
HE210401 <geamie@naver.com>
2023-05-13 21:06:24 +0900

    feat: 단계별(1) - [입출력과 사칙연산] - 12

---
+++ src/main/java/step_1/DrawCat_12.java
@@ -0,0 +1,10 @@
+package step_1;
+
+public class DrawCat_12 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
