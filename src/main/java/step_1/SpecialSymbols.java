48c968799b7632a48c6cf4a73bbd04ddd80ee999
HE210401 <geamie@naver.com>
2023-05-13 20:32:39 +0900

    feat: 단계별(1) - [입출력과 사칙연산] - 7

--- src/main/java/step_1/SpecialSymbols.java
+++ src/main/java/step_1/SpecialSymbols.java
@@ -1,8 +1,9 @@
+package step_1;
+
 import java.util.Scanner;
 
-public class Main {
+public class SpecialSymbols {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
         String id = sc.next().toLowerCase();
         System.out.println(id + "??!");
