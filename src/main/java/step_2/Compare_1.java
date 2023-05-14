98fe4fabf94f66856abb99a9f4657a87d85ad8f1
HE210401 <geamie@naver.com>
2023-05-14 10:50:58 +0900

    feat: [2단계] 조건문 - 1.두 수 비교

---
+++ src/main/java/step_2/Compare_1.java
@@ -0,0 +1,18 @@
+package step_2;
+
+import java.util.Scanner;
+
+public class Compare_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        String result = "";
+        if(a > b) result = ">";
+        else if(a < b) result = "<";
+        else result = "==";
+
+        System.out.println(result);
+    }
+}
