5bcc804a5893864c52760367ee5d66a05c1629e6
HE210401 <geamie@naver.com>
2023-06-01 13:53:00 +0900

    feat: [3단계] 반복문 - 4. 영수증

--- src/main/java/step_3/Receipt_4.java
+++ src/main/java/step_3/Receipt_4.java
@@ -1,7 +1,23 @@
 package step_3;
 
+import java.util.Scanner;
+
 public class Receipt_4 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int totalPrice = sc.nextInt();
+        int totalCount = sc.nextInt();
 
+        for(int i=0; i<totalCount; i++) {
+            int price = sc.nextInt();
+            int count = sc.nextInt();
+
+            totalPrice -= (price * count);
+        }
+
+        String isCorrected = "Yes";
+        if(totalPrice != 0) isCorrected = "No";
+
+        System.out.println(isCorrected);
     }
 }
