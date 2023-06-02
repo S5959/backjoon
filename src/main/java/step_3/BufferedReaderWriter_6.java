63f8392989e9f2c5c3b5861a58ed3357e2740ef9
HE210401 <geamie@naver.com>
2023-06-02 17:26:11 +0900

    feat: [3단계] 반복문 - 6. 빠른 A+B

---
+++ src/main/java/step_3/BufferedReaderWriter_6.java
@@ -0,0 +1,13 @@
+package step_3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BufferedReaderWriter_6 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int num1 = bf.read();
+
+    }
+}
