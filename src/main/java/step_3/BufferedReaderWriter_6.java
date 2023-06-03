9fe03e1548a1706bd5346ebcce158b0c7056881d
HE210401 <geamie@naver.com>
2023-06-03 17:07:52 +0900

    fix: [3단계] 반복문 - 6. 빠른 A+B

--- src/main/java/step_3/BufferedReaderWriter_6.java
+++ src/main/java/step_3/BufferedReaderWriter_6.java
@@ -1,13 +1,33 @@
 package step_3;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class BufferedReaderWriter_6 {
     public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        int num1 = bf.read();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int count = Integer.parseInt(br.readLine());
+
+        int[] result = new int[count];
+        for(int i=0; i<count; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+            int sum = 0;
+            while(st.hasMoreTokens()) {
+                sum += Integer.parseInt(st.nextToken());
+            }
+
+            result[i] = sum;
+        }
+
+        for(int sum : result) {
+            bw.write(sum);
+            bw.newLine();
+        }
+
+        br.close();
+        bw.close();
     }
 }
