e0247e89a6e48c4050fdabf1dd28c60b36dad881
HE210401 <geamie@naver.com>
2023-06-05 14:50:14 +0900

    feat: [3단계] 반복문 - 9. 별찍기 - 2 (좌우반전)

---
+++ src/main/java/step_3/DrawStarReverse_10.java
@@ -0,0 +1,26 @@
+package step_3;
+
+import java.io.*;
+
+public class DrawStarReverse_10 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int count = Integer.parseInt(in.readLine());
+
+        for (int i = 0; i < count; i++) {
+            for (int b = count; b > i+1; b--) {
+                out.write(" ");
+            }
+            for (int s = 0; s < i; s++) {
+                out.write("*");
+            }
+            out.newLine();
+        }
+
+        out.flush();
+        out.close();
+        in.close();
+    }
+}
