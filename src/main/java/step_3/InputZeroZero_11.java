fefda3470dd69e1a332fbe5f3c4ea3f27d671d8b
HE210401 <geamie@naver.com>
2023-06-06 14:13:59 +0900

    feat: [3단계] 반복문 - 10. A+B - 5(반복문 종료는 0 0)

---
+++ src/main/java/step_3/InputZeroZero_11.java
@@ -0,0 +1,27 @@
+package step_3;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class InputZeroZero_11 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        while (true) {
+            StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+
+            int num1 = Integer.parseInt(st.nextToken());
+            int num2 = Integer.parseInt(st.nextToken());
+
+            if(num1 == 0 && num2 == 0) break;
+
+            out.write(num1 + num2);
+            out.newLine();
+        }
+
+        out.flush();
+        out.close();
+        in.close();
+    }
+}
