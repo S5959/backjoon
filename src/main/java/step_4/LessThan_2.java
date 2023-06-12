0013c71029b02d2cf05861515a528e45893b117a
HE210401 <geamie@naver.com>
2023-06-12 18:01:26 +0900

    fix: [4단계] 1차원배열 - 2. X보다 작은 수

--- src/main/java/step_4/LessThan_2.java
+++ src/main/java/step_4/LessThan_2.java
@@ -1,7 +1,30 @@
 package step_4;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public class LessThan_2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // X보다 작은 수
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer tokenizer = new StringTokenizer(in.readLine(), " ");
+        if(tokenizer.countTokens() < 2) return;
+
+        int count = Integer.parseInt(tokenizer.nextToken());
+        int x = Integer.parseInt(tokenizer.nextToken());
+
+        tokenizer = new StringTokenizer(in.readLine(), " ");
+        if(tokenizer.countTokens() < count) return;
+
+        ArrayList<Integer> array = new ArrayList<>();
+        for(int i=0; i<count; i++) {
+            int y = Integer.parseInt(tokenizer.nextToken());
+            if(y < x) array.add(y);
+        }
+
+        System.out.println(array.toString());
     }
 }
