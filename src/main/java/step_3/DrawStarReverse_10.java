e9ddfa741650c50e64d7a290489ad61dc6f1444b
HE210401 <geamie@naver.com>
2023-06-15 18:07:07 +0900

    fix: [3단계] 1차원배열 - 10. 별찍기 -2

--- src/main/java/step_3/DrawStarReverse_10.java
+++ src/main/java/step_3/DrawStarReverse_10.java
@@ -4,15 +4,14 @@ import java.io.*;
 
 public class DrawStarReverse_10 {
     public static void main(String[] args) throws IOException {
-        // 또 오류남
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int count = Integer.parseInt(in.readLine());
 
         for (int i = 0; i < count; i++) {
-            for (int j = count-1; j > 0; j--) {
-                if(i<j) out.write(" ");
+            for (int j = count-1; j >= 0; j--) {
+                if(i < j) out.write(" ");
                 else out.write("*");
             }
             out.newLine();
