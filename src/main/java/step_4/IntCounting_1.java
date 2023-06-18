e11635427b6c5916f2f4aab66d89fe3bcf29ea3f
HE210401 <geamie@naver.com>
2023-06-18 22:54:49 +0900

    fix: [4단계] 1. 개수 세기

--- src/main/java/step_4/IntCounting_1.java
+++ src/main/java/step_4/IntCounting_1.java
@@ -1,7 +1,26 @@
 package step_4;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class IntCounting_1 {
-    public static void main(String[] args) {
-        // 1차원 배열
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(in.readLine());
+
+        StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+        if(st.countTokens() != size) return;
+
+        int search = Integer.parseInt(in.readLine());
+
+        int counting = 0;
+        while (st.hasMoreTokens()) {
+            int num = Integer.parseInt(st.nextToken());
+            if(num == search) counting++;
+        }
+
+        System.out.println(counting);
     }
 }
