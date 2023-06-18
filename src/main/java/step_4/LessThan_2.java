c1d8a5f8283f92b68c7c7c28f4a4ced6a6e1f8ab
HE210401 <geamie@naver.com>
2023-06-18 23:04:01 +0900

    fix: [4단계] 2. X보다 작은 수

--- src/main/java/step_4/LessThan_2.java
+++ src/main/java/step_4/LessThan_2.java
@@ -1,31 +1,27 @@
 package step_4;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class LessThan_2 {
     public static void main(String[] args) throws IOException {
-        // 오류나는 코드....고쳐야함
-        // X보다 작은 수
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter out = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer tokenizer = new StringTokenizer(in.readLine(), " ");
         if(tokenizer.countTokens() < 2) return;
 
-        int count = Integer.parseInt(tokenizer.nextToken());
+        int size = Integer.parseInt(tokenizer.nextToken());
         int x = Integer.parseInt(tokenizer.nextToken());
 
         tokenizer = new StringTokenizer(in.readLine(), " ");
-        if(tokenizer.countTokens() < count) return;
+        if(tokenizer.countTokens() < size) return;
 
-        ArrayList<Integer> array = new ArrayList<>();
-        for(int i=0; i<count; i++) {
-            int y = Integer.parseInt(tokenizer.nextToken());
-            if(y < x) array.add(y);
+        StringBuilder builder = new StringBuilder();
+        while (tokenizer.hasMoreTokens()) {
+            int num = Integer.parseInt(tokenizer.nextToken());
+            if(num < x) builder.append(num).append(" ");
         }
 
-        System.out.println(array.toString());
+        System.out.println(builder);
     }
 }
